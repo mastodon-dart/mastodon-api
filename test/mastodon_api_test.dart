a04cc6760133b099a4c4241e1b0eec4f0db92e5d
myConsciousness <kato.shinya.dev@gmail.com>
2022-11-20 16:28:20 +0900

    test: add tmp

--- test/mastodon_api_test.dart
+++ test/mastodon_api_test.dart
@@ -2,4 +2,8 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
 
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  test('', () {});
+}
