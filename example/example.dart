5ff8e5e06b4c57e748252246fab9e41598d745d2
myConsciousness <kato.shinya.dev@gmail.com>
2022-11-20 17:36:18 +0900

    docs: add infra docs

--- example/example.dart
+++ example/example.dart
@@ -5,7 +5,6 @@
 import 'package:mastodon_api/mastodon_api.dart';
 
 Future<void> main() async {
-  //  Coming soon...
   final mastodon = MastodonApi(
     instance: 'MASTODON_INSTANCE',
     bearerToken: 'BEARER_TOKEN',
